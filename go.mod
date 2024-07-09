fcf1ec5eaf5fbd4d7a33d7c2e09952dc7e845419
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2024-07-09 09:59:57 -0600

    Ultimo Commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/gambit
+
+go 1.22.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.30.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.27.24 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.24 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.32.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.1 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package gambit
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+	"github.com/gambit/awsgo"
+	"github.com/gambit/db"
+	"github.com/gambit/handlers"
+)
+func main(){
+	lambda.Start(EjecutoLambda)
+}
+
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest)(*events.APIGatewayProxyResponse, error){
+	//Conectar con el servicio de aws
+	awsgo.StartAWS()
+	if !ValidoParamtros() {
+		panic("Error en los parametros")
+		
+	}
+	var res *events.APIGatewayProxyResponse
+	prefijo := os.Getenv("UrlPrefix")
+	path:= strings.Replace(request.RawPath, prefijo,"",-1)
+	metodo := request.RequestContext.HTTP.Method
+	body := request.Body
+	header := request.Headers
+
+	db.ReadSecret()
+
+	//Llamar a los manejadores
+	status,message := handlers.Manejadores(path,metodo,body,header,request)
+	headersResp := map[string]string{
+		"Content-Type": "application/json",
+	}
+	res = &events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body: string(message),
+		Headers: headersResp,
+	}
+	return res,nil
+	
+
+}
+
+func ValidoParamtros() bool{
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro;
+	}
+	_,traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+		
+	}
+	return traeParametro
+}
